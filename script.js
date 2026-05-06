30493b78441ddc722bbd8fe02cd1264d2be37a9d
MohitMahmudgitup <syedmohitmahmudinzmaqm@gmail.com>
2026-05-06 13:10:44 +0600

    add examples for array filtering and reduce method in script.js

--- script.js
+++ script.js
@@ -29,4 +29,32 @@
 // console.log(u1)
 
 
+// -------------------03----------------
+
+// 01
+// let arr = [10, 15, 30,"c", "e", 75, 40 ,"q", 41, 23]
+// let elementN = arr.filter(item => typeof(item) === "number")
+// let elementS = arr.filter(item => typeof(item) === "string")
+// const NewArr = [elementN , elementS]
+// console.log(NewArr)
+
+// 02
+// let arr = [10, 15, 30,"c", "e", 75, 40 ,"q", 41, 23];
+// let num = []
+// let str = []
+// for (let i = 0 ; i < arr.length ; i ++ ){
+//     if(typeof(arr[i]) === "number"){
+//         num.push(arr[i])
+//     }else{
+//         str.push(arr[i])
+//     }
+// }
+// console.log([num,str])
+
+//03 ``Reduce method``
+
+// let num = [1,2,3,4];
+// let result = num.reduce((total , num)=> total + num , 0 );
+// console.log(result)
+
 
