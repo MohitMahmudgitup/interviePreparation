9941c9ee0650781df0a79a239c24eb844a5771d1
MohitMahmudgitup <syedmohitmahmudinzmaqm@gmail.com>
2026-05-06 12:09:30 +0600

    add global and constructor function examples in script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// -------------------01----------------
+// Globle Execution , Call Stack , Event loop , Queue
+
+// -------------------02----------------
+//Function Constructors || Class Constructors
+
+// ``Function Constructors``
+// function User (name,email,password){
+//     this.User = User,
+//     this.email = email,
+//     this.password = password,
+//     this.getFunc = ()=> console.log(User , email, password)
+// }
+
+// const u1 = new User('Mohit' , "mohit@gmail.com" , "Mohit12345Pas");
+// console.log(u1.email)
+
+// ``Class Constructors``
+// class User {
+//     constructor(User ,email, password ) {
+//         this.User = User,
+//         this.email = email,
+//         this.password = password,
+//         this.getFunc = () => console.log(User, email, password)
+//     }
+// }
+
+// const u1 = new User('Mohit' , "mohit@gmail.com" , "Mohit12345Pas");
+// console.log(u1)
+
+
+
