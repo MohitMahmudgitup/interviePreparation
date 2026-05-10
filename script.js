8d98de65439c336c52fabb8f0e95f3e2c4445bc4
MohitMahmudgitup <syedmohitmahmudinzmaqm@gmail.com>
2026-05-10 12:25:26 +0600

    add examples for object creation and asynchronous filtering in script.js

--- script.js
+++ script.js
@@ -57,4 +57,53 @@
 // let result = num.reduce((total , num)=> total + num , 0 );
 // console.log(result)
 
+//In JS everything is an object
+
+// const p1 = {
+//     fname : "Muhid",
+//     lname : 'Mahmud',
+//     getFullname(){
+//         return `${this.fname}${this.lname}`
+//     }
+// }
+
+// const p2 = Object.create(p1);
+// console.log(p2)
+
+
+
+// let data = [
+//   "item1", "item2", "item3", "item4", "item5",
+//   "item6", "item7", "item8", "item9", "item10",
+//   "item11", "item12", "item13", "item14", "item15",
+//   "item16", "item17", "item18", "item19", "item20",
+//   "item21", "item22", "item23", "item24", "item25",
+//   "item26", "item27", "item28", "item29", "item30",
+//   "item31", "item32", "item33", "item34", "item35",
+//   "item36", "item37", "item38", "item39", "item40",
+//   "item41", "item42", "item43", "item44", "item45",
+//   "item46", "item47", "item48", "item49", "item50"
+// ];
+// const search = 'this is me'
+
+// const fn1 = () => {
+
+//     if (!search) return;
+
+//     setTimeout(() => {
+//         const result = data.filter((item) =>
+//             item.includes(search)
+//         );
+//         console.log(result);
+//     }, 1000);
+// }
+// fn1()
+
+
+
+
+
+
+
+
 
